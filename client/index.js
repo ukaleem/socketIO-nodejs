33b16ba927f0e36e86f0da5bd224112568512d35
Kaleem Ullah <39130173+ukaleem@users.noreply.github.com>
2024-05-16 19:21:33 +0500

    Server setup

--- client/index.js
+++ client/index.js
@@ -1,13 +1,20 @@
-const socket = io("http://localhost:3000/");
-
-socket.on("connect", (socket)=>{
-    console.log("SOCKET RESPONSE : ",socket);
+const socket = io("http://localhost:4000");
+var resp;
+socket.on("connect", (sk)=>{
+    console.log(sk,socket);
+    socket.emit('message',"hii777777799999999")
 });
 
-socket.on("Message",(data)=>{
+socket.on("message",(data)=>{
     console.log("DATA from MESSAGE: ",data);
-    socket.emit("Message","All good bro hhh");
+    resp = data;
+    document.getElementById('messages').innerHTML = resp;
+    
+});
 
-})
+function sendMessage(ev) {
+    let messageVal = document.getElementById('messageRep');
+    console.log("im in",messageVal,ev);
+}
 
 
--- server/server.js
+++ server/server.js
@@ -4,21 +4,19 @@ const { Server } = require("socket.io");
 const httpServer = createServer();
 const socket = new Server(httpServer,{
     cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-        allowedHeaders: ["my-custom-header"],
-        credentials: true
+        origin: "*" //i wana fix cross oring issue 
     }
 }); 
 
-socket.on("connection", (socket)=>{
-    // console.log("SOCKET: ", socket);
-    socket.emit("Message","Hellow kaleem sab");
-    socket.on("Message",(data)=>{
-        console.log("CLIENT SAID: ", data)
+socket.on("connection", (sio)=>{
+    console.log("SOCKET: ", sio);
+
+    sio.emit("message","Hellow kaleem sab kesy ain");
+    sio.on("message",(data)=>{
+        console.log(data)
     });
 });
 
-httpServer.listen('3000', ()=>{
-    console.log("SERVER IS RUING ON PORT 3000"); 
+httpServer.listen('4000', ()=>{
+    console.log("SERVER IS RUING ON PORT 4000"); 
 });
